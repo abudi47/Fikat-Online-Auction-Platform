5d46644841a634609ca1ed587b7ca4b648fe8023
sonte adamu <142932810+sonteadamu@users.noreply.github.com>
2024-12-30 23:03:00 +0300

    Create Deals.java

---
+++ src/main/java/controller/user/Deals.java
@@ -0,0 +1,39 @@
+package controller.user;
+
+import java.io.IOException;
+import java.util.List;
+
+import beans.Item;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import service.ItemDAO;
+
+public class Deals extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+    public Deals() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sess = request.getSession();
+        if(sess.getAttribute("user") != null) {
+        	ItemDAO itemDAO = new ItemDAO();
+        	List<Item> items = itemDAO.getAll();
+        	request.setAttribute("items", items);
+        	System.out.println(items.size());
+            RequestDispatcher dis = request.getRequestDispatcher("WEB-INF/user/deals.jsp");
+            dis.forward(request, response);
+        } else {
+            response.sendRedirect("signin");
+        }
+	}
+}
